4175f7c03d0c07d893ff0c1c9bd5ad4bf1810cef
Stanley Chow <51177611+Stanley-C-02@users.noreply.github.com>
2024-11-24 20:12:46 -0500

    Refactor navigation bar into NavBar.jsx

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,83 +1,15 @@
-import { Link, NavLink, Outlet, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import styles from './root.module.css';
-import useSupabase from "../context/SupabaseContext";
+import Navbar from "../components/Navbar/Navbar";
 
 
 
 export default function Root() {
-    const { user, logout } = useSupabase();
-    const navigate = useNavigate();
-
-    const handleLogout = async () => {
-        try {
-            await logout();
-            navigate('/login');
-        } catch (error) {
-            console.error("Error logging out:", error); 
-        }
-    };
-
-
     return (
         <>
             <div className={styles["root-layout"]}>
                 <div className={styles.container}>
-                    <div className={styles.navbar}>
-                        <nav>
-                            <ul className={styles["nav-links"]}>
-                                <li>
-                                    {/* <img src="" alt="" className=[styles.logo}/> - logo placeholder */}
-                                    <NavLink to="/" className={styles["nav-item"]}>
-                                        <h1 className={styles.logo}>Travel Itinerary</h1>
-                                    </NavLink>
-                                </li>
-                                {
-                                    user && <>
-                                        <li>
-                                            <NavLink to="/saved-itineraries" className={styles["nav-item"]}>
-                                                Saved Itineraries
-                                            </NavLink>
-                                        </li>
-                                        <li>
-                                            <NavLink to="/admin-search" className={styles["nav-item"]}>
-                                                Admin Search
-                                            </NavLink>
-                                        </li>
-                                        <li>
-                                            <NavLink to="/my-itineraries" className={styles["nav-item"]}>
-                                                My Itineraries
-                                            </NavLink>
-                                        </li>
-                                        <li>
-                                            <NavLink to="/account" className={styles["nav-item"]}>
-                                                My Account
-                                            </NavLink>
-                                        </li>
-                                        <li>
-                                                <button className="logout"
-                                                    onClick={handleLogout}>
-                                                    Log Out
-                                                </button>
-                                            </li>
-                                    </>
-                                }
-                                {
-                                    !user && <>
-                                        <li>
-                                            <NavLink to="/login" className={styles["nav-item"]}>
-                                                Login
-                                            </NavLink>
-                                        </li>
-                                    </>
-                                }
-                                <li>
-                                    <NavLink to="/search-itinerary" className={styles["nav-item"]}>
-                                        Find Itineraries
-                                    </NavLink>
-                                </li>
-                            </ul>
-                        </nav>
-                    </div>
+                    <Navbar />
                     <main className={styles.main}>
                         <Outlet />
                     </main>
